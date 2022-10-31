fb3642fc7a25057d0708a0d294c4bd8c30494406
FranzRL <117094240+FranzRL@users.noreply.github.com>
2022-10-31 15:41:04 +0100

    du WIXXER 2.0 HOLO

--- app/src/main/java/com/example/all_in_one/MainActivity.java
+++ app/src/main/java/com/example/all_in_one/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    //Test Kommentar23 du WIXXER 2.0
+    //Test Kommentar23 du WIXXER 2.0 HOLO
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
